75a82e05bbcf8ea01cb6a11d6bb7e59e1fadcd10
dongsam <dongsamb@gmail.com>
2019-07-20 11:01:50 +0900

    WIP: Add boilerplate for fund module

---
+++ x/fund/internal/types/events.go
@@ -0,0 +1,11 @@
+package types
+
+// Fund module event types
+const (
+	EventTypeFund = ModuleName
+
+	AttributeKeyBondedRatio      = "bonded_ratio"
+	AttributeKeyInflation        = "inflation"
+	AttributeKeyAnnualProvisions = "annual_provisions"
+	AttributeKeyAmount           = "amount"
+)
