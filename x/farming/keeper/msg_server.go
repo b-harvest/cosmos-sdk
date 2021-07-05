05e92f1c78eea336dbd465ad779a3c1d1a2ed842
kogisin <jaybkim2@gmail.com>
2021-07-05 11:27:47 +0900

    update handler

--- x/farming/keeper/msg_server.go
+++ x/farming/keeper/msg_server.go
@@ -6,11 +6,10 @@ package keeper
 // so is not included in the coverage.
 
 import (
-	//"context"
-	//"fmt"
-	//"strconv"
-	//
-	//sdk "github.com/cosmos/cosmos-sdk/types"
+	"context"
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/cosmos/cosmos-sdk/x/farming/types"
 )
@@ -19,160 +18,45 @@ type msgServer struct {
 	Keeper
 }
 
-// NewMsgServerImpl returns an implementation of the distribution MsgServer interface
+// NewMsgServerImpl returns an implementation of the farming MsgServer interface
 // for the provided Keeper.
 func NewMsgServerImpl(keeper Keeper) types.MsgServer {
-	// TODO: unimplemented
-	//return &msgServer{Keeper: keeper}
-	return nil
+	return &msgServer{Keeper: keeper}
 }
 
-//var _ types.MsgServer = msgServer{}
+var _ types.MsgServer = msgServer{}
 
-//// Message server, handler for CreatePool msg
-//func (k msgServer) CreatePool(goCtx context.Context, msg *types.MsgCreatePool) (*types.MsgCreatePoolResponse, error) {
-//	ctx := sdk.UnwrapSDKContext(goCtx)
-//
-//	if k.GetCircuitBreakerEnabled(ctx) {
-//		return nil, types.ErrCircuitBreakerEnabled
-//	}
-//
-//	pool, err := k.Keeper.CreatePool(ctx, msg)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	ctx.EventManager().EmitEvents(sdk.Events{
-//		sdk.NewEvent(
-//			sdk.EventTypeMessage,
-//			sdk.NewAttribute(sdk.AttributeKeyModule, types.AttributeValueCategory),
-//		),
-//		sdk.NewEvent(
-//			types.EventTypeCreatePool,
-//			sdk.NewAttribute(types.AttributeValuePoolId, strconv.FormatUint(pool.Id, 10)),
-//			sdk.NewAttribute(types.AttributeValuePoolTypeId, fmt.Sprintf("%d", msg.PoolTypeId)),
-//			sdk.NewAttribute(types.AttributeValuePoolName, pool.Name()),
-//			sdk.NewAttribute(types.AttributeValueReserveAccount, pool.ReserveAccountAddress),
-//			sdk.NewAttribute(types.AttributeValueDepositCoins, msg.DepositCoins.String()),
-//			sdk.NewAttribute(types.AttributeValuePoolCoinDenom, pool.PoolCoinDenom),
-//		),
-//	})
-//
-//	return &types.MsgCreatePoolResponse{}, nil
-//}
-//
-//// Message server, handler for MsgDepositWithinBatch
-//func (k msgServer) DepositWithinBatch(goCtx context.Context, msg *types.MsgDepositWithinBatch) (*types.MsgDepositWithinBatchResponse, error) {
-//	ctx := sdk.UnwrapSDKContext(goCtx)
-//
-//	if k.GetCircuitBreakerEnabled(ctx) {
-//		return nil, types.ErrCircuitBreakerEnabled
-//	}
-//
-//	poolBatch, found := k.GetPoolBatch(ctx, msg.PoolId)
-//	if !found {
-//		return nil, types.ErrPoolBatchNotExists
-//	}
-//
-//	batchMsg, err := k.Keeper.DepositWithinBatch(ctx, msg)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	ctx.EventManager().EmitEvents(sdk.Events{
-//		sdk.NewEvent(
-//			sdk.EventTypeMessage,
-//			sdk.NewAttribute(sdk.AttributeKeyModule, types.AttributeValueCategory),
-//		),
-//		sdk.NewEvent(
-//			types.EventTypeDepositWithinBatch,
-//			sdk.NewAttribute(types.AttributeValuePoolId, strconv.FormatUint(batchMsg.Msg.PoolId, 10)),
-//			sdk.NewAttribute(types.AttributeValueBatchIndex, strconv.FormatUint(poolBatch.Index, 10)),
-//			sdk.NewAttribute(types.AttributeValueMsgIndex, strconv.FormatUint(batchMsg.MsgIndex, 10)),
-//			sdk.NewAttribute(types.AttributeValueDepositCoins, batchMsg.Msg.DepositCoins.String()),
-//		),
-//	})
-//
-//	return &types.MsgDepositWithinBatchResponse{}, nil
-//}
-//
-//// Message server, handler for MsgWithdrawWithinBatch
-//func (k msgServer) WithdrawWithinBatch(goCtx context.Context, msg *types.MsgWithdrawWithinBatch) (*types.MsgWithdrawWithinBatchResponse, error) {
-//	ctx := sdk.UnwrapSDKContext(goCtx)
-//
-//	if k.GetCircuitBreakerEnabled(ctx) {
-//		return nil, types.ErrCircuitBreakerEnabled
-//	}
-//
-//	poolBatch, found := k.GetPoolBatch(ctx, msg.PoolId)
-//	if !found {
-//		return nil, types.ErrPoolBatchNotExists
-//	}
-//
-//	batchMsg, err := k.Keeper.WithdrawWithinBatch(ctx, msg)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	ctx.EventManager().EmitEvents(sdk.Events{
-//		sdk.NewEvent(
-//			sdk.EventTypeMessage,
-//			sdk.NewAttribute(sdk.AttributeKeyModule, types.AttributeValueCategory),
-//		),
-//		sdk.NewEvent(
-//			types.EventTypeWithdrawWithinBatch,
-//			sdk.NewAttribute(types.AttributeValuePoolId, strconv.FormatUint(batchMsg.Msg.PoolId, 10)),
-//			sdk.NewAttribute(types.AttributeValueBatchIndex, strconv.FormatUint(poolBatch.Index, 10)),
-//			sdk.NewAttribute(types.AttributeValueMsgIndex, strconv.FormatUint(batchMsg.MsgIndex, 10)),
-//			sdk.NewAttribute(types.AttributeValuePoolCoinDenom, batchMsg.Msg.PoolCoin.Denom),
-//			sdk.NewAttribute(types.AttributeValuePoolCoinAmount, batchMsg.Msg.PoolCoin.Amount.String()),
-//		),
-//	})
-//
-//	return &types.MsgWithdrawWithinBatchResponse{}, nil
-//}
-//
-//// Message server, handler for MsgSwapWithinBatch
-//func (k msgServer) Swap(goCtx context.Context, msg *types.MsgSwapWithinBatch) (*types.MsgSwapWithinBatchResponse, error) {
-//	ctx := sdk.UnwrapSDKContext(goCtx)
-//
-//	if k.GetCircuitBreakerEnabled(ctx) {
-//		return nil, types.ErrCircuitBreakerEnabled
-//	}
-//
-//	params := k.GetParams(ctx)
-//	if msg.OfferCoinFee.IsZero() {
-//		msg.OfferCoinFee = types.GetOfferCoinFee(msg.OfferCoin, params.SwapFeeRate)
-//	}
-//
-//	poolBatch, found := k.GetPoolBatch(ctx, msg.PoolId)
-//	if !found {
-//		return nil, types.ErrPoolBatchNotExists
-//	}
-//
-//	batchMsg, err := k.Keeper.SwapWithinBatch(ctx, msg, types.CancelOrderLifeSpan)
-//	if err != nil {
-//		return &types.MsgSwapWithinBatchResponse{}, err
-//	}
-//
-//	ctx.EventManager().EmitEvents(sdk.Events{
-//		sdk.NewEvent(
-//			sdk.EventTypeMessage,
-//			sdk.NewAttribute(sdk.AttributeKeyModule, types.AttributeValueCategory),
-//		),
-//		sdk.NewEvent(
-//			types.EventTypeSwapWithinBatch,
-//			sdk.NewAttribute(types.AttributeValuePoolId, strconv.FormatUint(batchMsg.Msg.PoolId, 10)),
-//			sdk.NewAttribute(types.AttributeValueBatchIndex, strconv.FormatUint(poolBatch.Index, 10)),
-//			sdk.NewAttribute(types.AttributeValueMsgIndex, strconv.FormatUint(batchMsg.MsgIndex, 10)),
-//			sdk.NewAttribute(types.AttributeValueSwapTypeId, strconv.FormatUint(uint64(batchMsg.Msg.SwapTypeId), 10)),
-//			sdk.NewAttribute(types.AttributeValueOfferCoinDenom, batchMsg.Msg.OfferCoin.Denom),
-//			sdk.NewAttribute(types.AttributeValueOfferCoinAmount, batchMsg.Msg.OfferCoin.Amount.String()),
-//			sdk.NewAttribute(types.AttributeValueOfferCoinFeeAmount, batchMsg.Msg.OfferCoinFee.Amount.String()),
-//			sdk.NewAttribute(types.AttributeValueDemandCoinDenom, batchMsg.Msg.DemandCoinDenom),
-//			sdk.NewAttribute(types.AttributeValueOrderPrice, batchMsg.Msg.OrderPrice.String()),
-//		),
-//	})
-//
-//	return &types.MsgSwapWithinBatchResponse{}, nil
-//}
+// CreateFixedAmountPlan defines a method for creating fixed amount farming plan.
+func (k msgServer) CreateFixedAmountPlan(goCtx context.Context, msg *types.MsgCreateFixedAmountPlan) (*types.MsgCreateFixedAmountPlanResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	fmt.Println("ctx: ", ctx)
+	return &types.MsgCreateFixedAmountPlanResponse{}, nil
+}
+
+// CreateRatioPlan defines a method for creating ratio farming plan.
+func (k msgServer) CreateRatioPlan(goCtx context.Context, msg *types.MsgCreateRatioPlan) (*types.MsgCreateRatioPlanResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	fmt.Println("ctx: ", ctx)
+	return &types.MsgCreateRatioPlanResponse{}, nil
+}
+
+// Stake defines a method for staking coins to the farming plan.
+func (k msgServer) Stake(goCtx context.Context, msg *types.MsgStake) (*types.MsgStakeResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	fmt.Println("ctx: ", ctx)
+	return &types.MsgStakeResponse{}, nil
+}
+
+// Unstake defines a method for unstaking coins from the farming plan.
+func (k msgServer) Unstake(goCtx context.Context, msg *types.MsgUnstake) (*types.MsgUnstakeResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	fmt.Println("ctx: ", ctx)
+	return &types.MsgUnstakeResponse{}, nil
+}
+
+// Claim defines a method for claiming farming rewards from the farming plan.
+func (k msgServer) Claim(goCtx context.Context, msg *types.MsgClaim) (*types.MsgClaimResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	fmt.Println("ctx: ", ctx)
+	return &types.MsgClaimResponse{}, nil
+}
