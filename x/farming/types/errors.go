e1724eb163fa36f818b73c92032b24509e4661ea
kogisin <jaybkim2@gmail.com>
2021-07-05 11:08:10 +0900

    fix sentinel error to start from 2 as a code value of one is reserved for internal errors - https://docs.cosmos.network/v0.42/building-modules/errors.html#registration

--- x/farming/types/errors.go
+++ x/farming/types/errors.go
@@ -6,9 +6,10 @@ import (
 
 // farming module sentinel errors
 var (
-	ErrPlanNotExists             = sdkerrors.Register(ModuleName, 1, "plan not exists")
-	ErrPlanTypeNotExists         = sdkerrors.Register(ModuleName, 2, "plan type not exists")
-	ErrInvalidFarmingPoolAddress = sdkerrors.Register(ModuleName, 3, "invalid farming pool address")
+	ErrPlanNotExists             = sdkerrors.Register(ModuleName, 2, "plan not exists")
+	ErrPlanTypeNotExists         = sdkerrors.Register(ModuleName, 3, "plan type not exists")
+	ErrInvalidFarmingPoolAddress = sdkerrors.Register(ModuleName, 4, "invalid farming pool address")
+	ErrInvalidFarmerAddress      = sdkerrors.Register(ModuleName, 5, "invalid farmer address")
 
 	// TODO: TBD more err types for farming module
 )
