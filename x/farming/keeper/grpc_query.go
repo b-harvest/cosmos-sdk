751a8b19a2091aec5266eeb9088dcf00307ab6ef
Hanjun Kim <hallazzang@gmail.com>
2021-07-06 13:28:56 +0900

    remove dead comment

--- x/farming/keeper/grpc_query.go
+++ x/farming/keeper/grpc_query.go
@@ -1,8 +1,5 @@
 package keeper
 
-// DONTCOVER
-// client is excluded from test coverage in the poc phase milestone 1 and will be included in milestone 2 with completeness
-
 import (
 	"github.com/cosmos/cosmos-sdk/x/farming/types"
 )
